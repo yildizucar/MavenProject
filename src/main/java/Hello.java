6ada91146841e2ea9b74508356d25dc13c9adac1
yildiz ucar <yildizucr@gmail.com>
2021-05-22 00:14:58 -0400

    added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing hello feature");
+        System.out.println("GitHub repo is already piblished to GitHub");
+        System.out.println("Doing more work");
     }
 }
